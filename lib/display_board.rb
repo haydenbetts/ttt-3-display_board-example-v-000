56d0b97a81308f47e3ef5e70a2e9ba2937dc7f5a
Hayden Betts <haydenbetts@gmail.com>
2018-06-20 21:32:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,7 @@
 def display_board
   puts "   |   |   "
   puts "-----------"
+  puts "   |   |   "
   expected_output += "-----------\n"
   expected_output += "   |   |   \n"
   expected_output += "-----------\n"
